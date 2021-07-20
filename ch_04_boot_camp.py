2628d267360c44e21ca85e38c4a3cb7731b6860c
i323878 <anurag.negi@sap.com>
2021-07-20 12:33:48 +0530

    code(prev): bit parity

--- ch_04_boot_camp.py
+++ ch_04_boot_camp.py
@@ -1,8 +1,25 @@
+def opt_parity(x):
+    result = 0
+    while x:
+        result ^= 1
+        x &= x-1
+    
+    return result
+
+def parity(x):
+    parity = 0
+    while x:
+        parity ^= x & 1
+        x >>= 1
+    return parity
+
 def number_to_binary(x):
     b_str = ""
+    if(x==0):
+        return "0"
     while x:
         b_str += str(x&1)
-        x >>= 1
+        x >>= 10
     return b_str[::-1]
 
 def count_bits(x):
@@ -14,7 +31,7 @@ def count_bits(x):
     return num_bits
 
 for i in range(20):
-    print("Number is {}, binary is {} and bit-count is {}".format(i,number_to_binary(i),count_bits(i)))
+    print("Number is {}, binary: {}, bit-count: {},  parity: {}, opt_parity: {}".format(i,number_to_binary(i),count_bits(i),parity(i), opt_parity(i)))
 
 
 # Fun Shuffle
