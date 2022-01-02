1cc927ceae0ee8a4fe57df9bc39a83a20a405ba6
Anurag <anurag.negi1729@gmail.com>
2022-01-02 21:46:03 +0530

    code(function): function added

--- C++/p04.cpp
+++ C++/p04.cpp
@@ -19,9 +19,17 @@ void fun_swap_without_var(int a, int b){
     cout<<"Numbers are: "<<a<<" and "<<b<< endl;
 }
 
+void fun_swap_without_var_tersely(int a, int b){
+    cout<<"Numbers are: "<<a<<" and "<<b<< endl;
+    a ^= b ^= a ^= b;
+    cout<<"Numbers are: "<<a<<" and "<<b<< endl;
+}
+
 int main(){
     cout<<"---With variables---"<<endl;
     fun_swap(4,3);
     cout<<"\n---Without variables---"<<endl;
     fun_swap_without_var(56,43);
+    cout<<"\n---Without variables Tersely---"<<endl;
+    fun_swap_without_var_tersely(232,123);
 }
