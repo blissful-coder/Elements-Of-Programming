4624335c1200690eeac7607b8ef5ca9c37d82c53
i323878 <anurag.negi@sap.com>
2022-01-02 21:18:20 +0530

    code(refactor: different folders based for language

---
+++ C++/p04.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void fun_swap(int a, int b){
+    cout<<"Numbers are: "<<a<<" and "<<b<< endl;
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout<<"Numbers are: "<<a<<" and "<<b<< endl;
+}
+
+void fun_swap_without_var(int a, int b){
+    cout<<"Numbers are: "<<a<<" and "<<b<< endl;
+    a = a^b;
+    b = a^b;
+    a = a^b;
+    cout<<"Numbers are: "<<a<<" and "<<b<< endl;
+}
+
+int main(){
+    cout<<"---With variables---"<<endl;
+    fun_swap(4,3);
+    cout<<"\n---Without variables---"<<endl;
+    fun_swap_without_var(56,43);
+}
