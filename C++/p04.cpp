143efab13416e57c8a141dcab57c82d66e5736d1
Anurag <anurag.negi1729@gmail.com>
2022-01-09 18:29:53 +0530

    code(c++): code to practice c++

--- C++/p04.cpp
+++ C++/p04.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+void pdt_size(){
+    cout << "Size of char : " << sizeof(char)<< " byte" << endl;
+    cout << "Size of int : " << sizeof(int)<< " bytes" << endl;
+    cout << "Size of short int : " << sizeof(short int)<< " bytes" << endl;
+    cout << "Size of long int : " << sizeof(long int) << " bytes" << endl;
+    cout << "Size of signed long int : " << sizeof(signed long int) << " bytes" << endl;
+    cout << "Size of unsigned long int : " << sizeof(unsigned long int) << " bytes" << endl;
+    cout << "Size of long long : " << sizeof(long long) << " bytes" <<endl;
+    cout << "Size of float : " << sizeof(float) << " bytes" <<endl;
+    cout << "Size of double : " << sizeof(double) << " bytes" << endl;
+    cout << "Size of wchar_t : " << sizeof(wchar_t) << " bytes" <<endl;
+
+}
+
 void fun_swap(int a, int b){
     cout<<"Numbers are: "<<a<<" and "<<b<< endl;
     int temp;
@@ -30,6 +44,8 @@ int main(){
     fun_swap(4,3);
     cout<<"\n---Without variables---"<<endl;
     fun_swap_without_var(56,43);
-    cout<<"\n---Without variables Tersely---"<<endl;
+    cout<<"\n---Without variables Tersely---"<<endl;    
     fun_swap_without_var_tersely(232,123);
+    cout<<"\n---Data Type size---"<<endl;
+    pdt_size();
 }
