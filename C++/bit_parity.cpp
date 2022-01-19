b36fce70aeccef80f2aa1f53d5d20d07eb82ef04
Anurag <anurag.negi1729@gmail.com>
2022-01-19 12:37:11 +0530

    code(practice): testing bitset

--- C++/bit_parity.cpp
+++ C++/bit_parity.cpp
@@ -23,6 +23,6 @@ short Parity2(unsigned long x){
 
 int main(){
     unsigned long val = 8;
-    cout<< "Parity of number: " << bitset<8>(21) << " is " << Parity1(21) << endl;
-    cout<< "Parity of number: " << bitset<8>(21) << " is " << Parity2(21) << endl;
+    cout<< "Parity of number: " << bitset<16>(21) << " is " << Parity1(21) << endl;
+    cout<< "Parity of number: " << bitset<16>(21) << " is " << Parity2(21) << endl;
 }
