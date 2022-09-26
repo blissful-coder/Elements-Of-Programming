7e9458722064afc51df4cbb842325cc9891c4ef5
Anurag <anurag.negi1729@gmail.com>
2022-09-26 13:38:58 +0530

    chore(ci): linked list in golang

--- golang/linkedlist.go
+++ golang/linkedlist.go
@@ -2,36 +2,54 @@ package main
 
 import "fmt"
 
-type person struct {
-	name string
-	age  int
+type Node struct {
+	value int
+	next  *Node
 }
 
-func newPerson(name string) *person {
+type LinkedList struct {
+	head *Node
+	len  int
+}
 
-	p := person{name: name}
-	p.age = 42
-	return &p
+func (l *LinkedList) Insert(val int) {
+	n := Node{}
+	n.value = val
+	if l.len == 0 {
+		l.head = &n
+		l.len++
+		return
+	}
+	ptr := l.head
+	for i := 0; i < l.len; i++ {
+		if ptr.next == nil {
+			ptr.next = &n
+			l.len++
+			return
+		}
+		ptr = ptr.next
+	}
+}
+
+func (l *LinkedList) Print() {
+	if l.len == 0 {
+		fmt.Println("No nodes in list")
+	}
+	ptr := l.head
+	for i := 0; i < l.len; i++ {
+		fmt.Println("Node: ", ptr)
+		ptr = ptr.next
+	}
 }
 
 func main() {
+	l := LinkedList{}
 
-	fmt.Println(person{"Bob", 20})
-
-	fmt.Println(person{name: "Alice", age: 30})
-
-	fmt.Println(person{name: "Fred"})
-
-	fmt.Println(&person{name: "Ann", age: 40})
-
-	fmt.Println(newPerson("Jon"))
-
-	s := person{name: "Sean", age: 50}
-	fmt.Println(s.name)
-
-	sp := &s
-	fmt.Println(sp.age)
-
-	sp.age = 51
-	fmt.Println(sp.age)
+	fmt.Println("\n************* Insert *************")
+	l.Insert(89)
+	l.Insert(232)
+	l.Insert(1)
+	l.Insert(95)
+	fmt.Println("************* Print *************")
+	l.Print()
 }
